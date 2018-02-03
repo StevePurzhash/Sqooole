2a8434572d7ef7579d3451a5b143fc7c9534c6c8
StevePurzhash <devolper7@gmail.com>
2018-02-04 01:01:45 +0200

    Fixed views

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         
+  has_many :shots, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  acts_as_voter
+end
